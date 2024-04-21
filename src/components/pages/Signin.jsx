3446a70779d9a0ad39e48a8d833b2240d6616b61
azamov-asadbek271 <azamjonovasa45@gmail.com>
2024-04-21 07:20:24 +0300

    login

--- src/components/pages/Signin.jsx
+++ src/components/pages/Signin.jsx
@@ -1,14 +1,27 @@
 import { useSignup } from "../hooks/useSignup";
 import { ImGoogle } from "react-icons/im";
+import { Link } from "react-router-dom";
+
 function Signin() {
   const { singupWithGoggle } = useSignup();
 
   return (
     <div className="min-h-screen grid place-items-center">
-      <button onClick={singupWithGoggle} className="btn btn-secondary">
-        <ImGoogle className="text-2xl" />
-        <span className="text-2xl">Google</span>
-      </button>
+      <div className="max-w-96 w-full">
+        <button
+          onClick={singupWithGoggle}
+          className="btn btn-secondary w-full mb-5"
+        >
+          <ImGoogle className="text-2xl" />
+          <span className="text-2xl">Google</span>
+        </button>
+        <p className="text-center">
+          If you don't have account
+          <Link className="link ml-1 text-cyan-600" to="/signup">
+            Signup
+          </Link>
+        </p>
+      </div>
     </div>
   );
 }
--- src/components/pages/Signup.jsx
+++ src/components/pages/Signup.jsx
@@ -1,14 +1,20 @@
 import { useSignup } from "../hooks/useSignup";
 import { ImGoogle } from "react-icons/im";
+import { Link } from "react-router-dom";
 
 function Signup() {
   const { singupWithGoggle } = useSignup();
   return (
     <div className="min-h-screen grid place-items-center">
-      <button onClick={singupWithGoggle} className="btn btn-secondary">
-        <ImGoogle className="text-2xl" />
-        <span className="text-2xl">Google</span>
-      </button>
+      <div className="max-w-96 w-full">
+        <button onClick={singupWithGoggle} className="btn btn-secondary w-full mb-5">
+          <ImGoogle className="text-2xl" />
+          <span className="text-2xl">Google</span>
+        </button>
+        <p className="text-center">
+          Are you already registered ? <Link className="link text-cyan-600" to="/signin">Login</Link>
+        </p>
+      </div>
     </div>
   );
 }
